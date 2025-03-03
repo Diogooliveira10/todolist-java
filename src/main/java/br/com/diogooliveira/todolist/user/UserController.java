ff1e697406573d34940b7434789e1f47d8a7bddd
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-03-03 17:18:30 -0300

    feat: Using ResponseEntity

--- src/main/java/br/com/diogooliveira/todolist/user/UserController.java
+++ src/main/java/br/com/diogooliveira/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.diogooliveira.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,15 +38,16 @@ public class UserController {
     * dentro do corpo da requisição -> @RequestBody
     */
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null) {
-            System.out.println("User already exists");
-            return null;
+            // Mensagem de erro
+            // Status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
